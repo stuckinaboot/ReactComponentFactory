51a5a4a151cd53010cc6341b73dd4b410a8808f5
Aspyn Palatnick <paluter@yahoo.com>
2020-12-25 15:43:49 -0500

    clean up

--- factory/scripts/preparePackage.sh
+++ factory/scripts/preparePackage.sh
@@ -2,7 +2,8 @@
 
 DEV_DIR=$1
 COMPONENT_DEV_DIR="src/components/$DEV_DIR"
-NPM_DIST_DIR="factory/boilerplate/npm_base"
+NPM_BOILERPLATE_DIR="factory/boilerplate/npm_base"
+DIST_DIR="npm_dist/$DEV_DIR"
 
 # Check if a component for $DEV_DIR exists
 if [ -d $COMPONENT_DEV_DIR ] 
@@ -18,25 +19,25 @@ if [ ! -f $COMPONENT_DEV_DIR/package.json ]; then
     exit 1
 fi
 
-if [ -d $DEV_DIR ] 
+if [ -d $DIST_DIR ] 
 then
-    echo "Directory $DEV_DIR exists." 
+    echo "Directory $DIST_DIR exists." 
 else
-    echo "Directory $DEV_DIR does not exist, creating directory $DEV_DIR"
-    mkdir $DEV_DIR
-    echo "Copying base npm package at $NPM_DIST_DIR into $DEV_DIR"
-    cp -r $NPM_DIST_DIR/ $DEV_DIR/
+    echo "Directory $DIST_DIR does not exist, creating directory $DIST_DIR"
+    mkdir $DIST_DIR
+    echo "Copying base npm package at $NPM_BOILERPLATE_DIR into $DIST_DIR"
+    cp -r $NPM_BOILERPLATE_DIR/ $DIST_DIR/
 fi
 
-echo "Clearing existing $DEV_DIR/src"
-rm -rf $DEV_DIR/src/*
+echo "Clearing existing $DIST_DIR/src"
+rm -rf $DIST_DIR/src/*
 
-echo "Copying src/components/$DEV_DIR to $DEV_DIR/src"
-cp -r $COMPONENT_DEV_DIR/ $DEV_DIR/src/
+echo "Copying src/components/$DEV_DIR to $DIST_DIR/src"
+cp -r $COMPONENT_DEV_DIR/ $DIST_DIR/src/
 
-echo "Moving src/components/$DEV_DIR/package.json to $DEV_DIR"
-cd $DEV_DIR/src
+echo "Moving src/components/$DEV_DIR/package.json to $DIST_DIR"
+cd $DIST_DIR/src
 mv package.json ..
 
-echo "Successfully copied files to $DEV_DIR/src"
-echo "Next steps: Edit $DEV_DIR/package.json, in $DEV_DIR run yarn and yarn build, and finally npm publish when you're ready"
+echo "Successfully copied files to $DIST_DIR/src"
+echo "Next steps: Edit $DIST_DIR/package.json, in $DIST_DIR run yarn and yarn build, and finally npm publish when you're ready"
