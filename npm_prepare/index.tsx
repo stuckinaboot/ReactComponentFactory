effda6790c536cd8b9a4900052c9d0acfd80d111
Aspyn Palatnick <paluter@yahoo.com>
2020-12-25 14:41:19 -0500

    add npm_prepare

---
+++ npm_prepare/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import { ComponentProps } from "./typings";
+
+export default function MyComponent(props: ComponentProps): React.ReactElement {
+  return <></>;
+}
---
+++ npm_prepare/typings/index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Typings
+ */
+
+export type ComponentProps = {};
