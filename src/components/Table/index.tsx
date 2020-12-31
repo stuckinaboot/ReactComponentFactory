5620ad2c100219fbaaaf6a874f97dd1500bc8d61
Aspyn Palatnick <paluter@yahoo.com>
2020-12-31 18:40:14 -0500

    clean

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,35 +1,61 @@
 import * as React from "react";
-import MuiTable from "mui-virtualized-table";
-import { AutoSizer } from "react-virtualized";
+import MUIDataTable from "mui-datatables";
 
-// MuiTable reference
-// https://github.com/techniq/mui-virtualized-table
+const columns = [
+  {
+    name: "name",
+    label: "Name",
+    options: {
+      filter: true,
+      sort: true,
+    },
+  },
+  {
+    name: "company",
+    label: "Company",
+    options: {
+      filter: true,
+      sort: false,
+    },
+  },
+  {
+    name: "city",
+    label: "City",
+    options: {
+      filter: true,
+      sort: false,
+    },
+  },
+  {
+    name: "state",
+    label: "State",
+    options: {
+      filter: true,
+      sort: false,
+    },
+  },
+];
 
-https: export type Column = { name: string; header: string };
+const data = [
+  { name: "Joe James", company: "Test Corp", city: "Yonkers", state: "NY" },
+  { name: "John Walsh", company: "Test Corp", city: "Hartford", state: "CT" },
+  { name: "Bob Herm", company: "Test Corp", city: "Tampa", state: "FL" },
+  { name: "James Houston", company: "Test Corp", city: "Dallas", state: "TX" },
+];
 
 export default function Table(props: {
-  columns: Column[];
+  columns: string[];
   // Data to render using defined columns.
   // For each element in data, there should be values for
   // some "name"s from columns
   data: any[];
 }): React.ReactElement {
   return (
-    <AutoSizer>
-      {({ width, height }) => (
-        <MuiTable
-          height={height}
-          columns={props.columns.map(({ name, header }, idx) => ({
-            name,
-            header,
-            cellProps:
-              idx === 0 ? { style: { paddingRight: 0 } } : { align: "right" },
-          }))}
-          width={width}
-          data={props.data}
-          includeHeaders={true}
-        />
-      )}
-    </AutoSizer>
+    <MUIDataTable
+      title={"Employee List"}
+      data={data}
+      columns={columns}
+      options={{ filterType: "textField" }}
+    />
   );
 }
